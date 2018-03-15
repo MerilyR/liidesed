5a529cd0e7035bf919490632d6800bcaf682a7e1
MerilyR <merily.r@gmail.com>
2018-03-15 09:49:21 +0200

    Turtle moving
    Direction by int

--- src/main/java/ee/tlu/automaattestid/Turtle.java
+++ src/main/java/ee/tlu/automaattestid/Turtle.java
@@ -1,15 +1,53 @@
 package ee.tlu.automaattestid;
+
 /**
  *
  * @author Merily.Rooparg
  */
 public class Turtle {
 
-    int x, y;
+    /*
+    public enum Angle {
+        right, down, left, up;
+        public Angle next() {
+            switch (this) {
+                case right  : return down;
+                case down   : return left;
+                case left   : return up;
+                case up     : return right;
+                default: return null;
+            }
+        }
+        public Angle previous() {
+            switch (this) {
+                case right  : return up;
+                case down   : return right;
+                case left   : return down;
+                case up     : return left;
+                default: return null;
+            }
+        }
+        private static Angle[] vals = values();
+        public Angle nextAngle() {
+            return vals[(this.ordinal()+1) % vals.length];
+        }
+        public Angle previousAngle() {
+            if (this.ordinal () == 0)
+                return vals [vals.length - 1];
+            return vals[(this.ordinal()-1) % vals.length];
+        }
+    };
+    */
+    
+//    Angle angle;
+    int direction;
+    private int x, y;
     
     public Turtle(int tx, int ty) {
         x = tx;
         y = ty;
+        direction = 1;
+//        angle = Angle.right;
     }
 
     public int getX() {
@@ -20,6 +58,49 @@ public class Turtle {
         return y;
     }
     
+    public int getDirection(){
+        return direction;
+    }
     
+//    public Angle getAngle() {
+//        System.out.println("Angle - "+angle);
+//        return angle;
+//    }
+
+    public void rotateRight() {
+//        angle = angle.next();
+//        angle = angle.nextAngle();
+        direction ++;
+        if (direction > 4)
+            direction = direction - 4;
+    }
+    
+    public void rotateLeft() {
+//        angle = angle.previous();
+//        angle = angle.previousAngle();
+        direction --;
+        if (direction < 1)
+            direction = direction + 4;
+    }
+    
+    /**
+     * Take 1 step in direction
+     */
+    public void step () {
+        switch(direction) {
+            case 1:
+                x ++;
+                break;
+            case 2:
+                y ++;
+                break;
+            case 3:
+                x --;
+                break;
+            case 4: 
+                y --;
+                break;
+        }
+    }
     
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -6,8 +6,10 @@
 
 
 import ee.tlu.automaattestid.Turtle;
+//import ee.tlu.automaattestid.Turtle.Angle;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -21,10 +23,57 @@ public class FirstTest {
     public void first() {
         assertEquals(5, 3+2);
     }
+    Turtle t;
+    
+    @Before
+    public void setUp() {
+        t = new Turtle (3, 5);
+        System.out.println("Turtle created with parameters "+t.getX() + " & "+t.getY());
+    }
+    
     @Test
     public void second(){
-        Turtle t = new Turtle (3, 5);
         assertEquals (3, t.getX());
         assertEquals (5, t.getY());
+        assertEquals (1, t.getDirection());
+    }
+    @Test
+    public void turnRightTest() {
+        assertEquals (1, t.getDirection());
+//        assertEquals (Angle.right, t.getAngle());
+        t.rotateRight();
+        assertEquals (2, t.getDirection());
+//        assertEquals (Angle.down, t.getAngle());
+        t.rotateRight();
+        assertEquals (3, t.getDirection());
+        t.rotateRight();
+        assertEquals (4, t.getDirection());
+//        assertEquals (Angle.up, t.getAngle());
+        t.rotateRight();
+        assertEquals (1, t.getDirection());
+    }
+    @Test
+    public void turnLefttTest() {
+        assertEquals (1, t.getDirection());
+        t.rotateLeft();
+        assertEquals (4, t.getDirection());
+//        assertEquals (Angle.up, t.getAngle());
+        t.rotateLeft();
+        assertEquals (3, t.getDirection());
+        t.rotateLeft();
+        assertEquals (2, t.getDirection());
+//        assertEquals (Angle.down, t.getAngle());
+        t.rotateLeft();
+        assertEquals (1, t.getDirection());
+    }
+    
+    @Test public void moveTest() {
+        assertEquals (3, t.getX());
+        assertEquals (5, t.getY());
+        assertEquals (1, t.getDirection());
+        t.step();
+        assertEquals (4, t.getX());
+        assertEquals (5, t.getY());
+        assertEquals (1, t.getDirection());
     }
 }
